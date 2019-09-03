b0a45d4a8da9ecf0d819289c4140f984d9fe6831
Mofrad <ali.mofrad@yahoo.com>
2019-09-03 21:23:20 +0430

    Update translator.ts : log untranslated keys Update fa-IR.ts : add more words

--- public/app/locale/translator.ts
+++ public/app/locale/translator.ts
@@ -1,20 +1,32 @@
 import { vocabs } from "./fa-IR";
 
 interface Vocabs {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 export function Translator(vocabulary: Vocabs) {
+  const dictionary = Object.entries(vocabulary).reduce(
+    (dic, [key, value]) => ({
+      ...dic,
+      [key.toLowerCase()]: value,
+    }),
+    {} as Vocabs
+  );
 
-    const dictionary = Object.entries(vocabulary).reduce((dic, [key, value]) => ({
-        ...dic,
-        [key.toLowerCase()]: value,
-    }), {} as Vocabs);
-
-    return function translate(key = "") {
-        const term = key.toLowerCase();
-        return dictionary[term] || key;
-    };
+  return function translate(key = "") {
+    if (typeof key !== "string") {
+      console.log("delete -> " + key);
+      console.log(new Error().stack);
+      return key;
+    }
+    const term = key.toLowerCase();
+    if (!dictionary[term]) {
+      console.log("add -> " + key);
+      console.log(new Error().stack);
+      return key;
+    }
+    return dictionary[term] || key;
+  };
 }
 
 export const translate = Translator(vocabs);
